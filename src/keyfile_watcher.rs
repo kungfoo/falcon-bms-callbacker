37bb47ff273663fe1354d816b4a49e955115a009
kungfoo <falconbmscontrol@gmail.com>
2024-09-26 15:50:30 +0200

    Only read the file for the first time and when it's contents change

--- src/keyfile_watcher.rs
+++ src/keyfile_watcher.rs
@@ -5,6 +5,8 @@ use log::*;
 use std::error::Error;
 use std::fs::File;
 use std::io;
+use std::io::Read;
+use std::io::Seek;
 use std::path::Path;
 use std::time::Duration;
 use tokio::sync::mpsc::Sender;
@@ -12,37 +14,70 @@ use tokio::time::sleep;
 
 pub struct KeyfileWatcher {
     tx: Sender<Message>,
+    last_hash: Option<u64>,
 }
 
 impl KeyfileWatcher {
     pub async fn new(tx: Sender<Message>) -> Result<KeyfileWatcher, io::Error> {
-        Ok(KeyfileWatcher { tx })
+        Ok(KeyfileWatcher {
+            tx,
+            last_hash: None,
+        })
     }
 
     pub async fn run(self) -> Result<(), Box<dyn Error + Send + Sync>> {
-        let KeyfileWatcher { tx } = self;
+        let KeyfileWatcher { tx, mut last_hash } = self;
 
         loop {
             if let Ok(string_data) = StringData::read() {
-                info!("BMS appears to be running...");
+                debug!("BMS appears to be running...");
                 let key_file_path = &string_data[&StringId::KeyFile];
 
                 if key_file_path.len() > 0 {
-                    debug!("About to read key file: {:?}", key_file_path);
+                    trace!("About to read key file: {:?}", key_file_path);
                     let path = Path::new(key_file_path);
-                    let file = File::open(&path).unwrap();
+                    let mut file = File::open(&path).unwrap();
                     let file_name = String::from(path.file_name().unwrap().to_str().unwrap());
-                    match falcon_key_file::parse(file_name, &file) {
-                        Ok(key_file) => {
-                            let message = Message::KeyfileRead { key_file };
-                            tx.send(message).await.unwrap();
+
+                    let mut buffer = Vec::new();
+                    buffer.clear();
+                    let _ = file.read_to_end(&mut buffer);
+
+                    let hash = seahash::hash(&buffer);
+
+                    match last_hash {
+                        Some(old_hash) => {
+                            if hash != old_hash {
+                                debug!("Key file contents changed, re-reading it.");
+                                Self::read_key_file_and_send_result(file_name, &file, &tx).await;
+                                last_hash = Some(hash);
+                            }
                         }
-                        Err(e) => error!("Could not read key file: {:?}", e),
+                        None => {
+                            debug!("Reading key file for the first time.");
+                            Self::read_key_file_and_send_result(file_name, &file, &tx).await;
+                            last_hash = Some(hash);
+                        }
                     }
                 }
             }
 
             sleep(Duration::from_secs(10)).await;
+        }
+    }
+
+    async fn read_key_file_and_send_result(
+        file_name: String,
+        mut file: &File,
+        tx: &Sender<Message>,
+    ) {
+        file.rewind().expect("Could not seek to beginning of file.");
+        match falcon_key_file::parse(file_name, &file) {
+            Ok(key_file) => {
+                let message = Message::KeyfileRead { key_file };
+                tx.send(message).await.unwrap();
+            }
+            Err(e) => error!("Could not read key file: {:?}", e),
         }
     }
 }
