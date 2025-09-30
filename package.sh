da3e189b53ab67d738dfd6cc0239468d5098918c
Silvio Heuberger <falconbmscontrol@gmail.com>
2025-09-30 09:12:55 +0200

    Do not package i686 binaries for now

--- package.sh
+++ package.sh
@@ -4,25 +4,26 @@ set -xe
 version=`git describe --tags --always --dirty`
 echo "Packaging ${version}..."
 
-cargo build --release
+cargo build --release --target x86_64-pc-windows-gnu
 rm -rf target/package/
 mkdir -p target/package/win64/
-cp target/release/falcon_bms_callbacker.exe target/package/win64/falcon_bms_callbacker-win64-${version}.exe
+upx --best target/x86_64-pc-windows-gnu/release/falcon_bms_callbacker.exe
+cp target/x86_64-pc-windows-gnu/release/falcon_bms_callbacker.exe target/package/win64/falcon_bms_callbacker-win64-${version}.exe
 cp config-release.toml target/package/win64/config.toml
 
 pushd target/package/win64/
 zip -9 falcon_bms_callbacker-win64-${version}.zip *
 popd
 
-cargo build --target=i686-pc-windows-msvc --release
+#cargo build --target=i686-pc-windows-msvc --release
 
-mkdir -p target/package/win32/
-cp target/i686-pc-windows-msvc/release/falcon_bms_callbacker.exe target/package/win32/falcon_bms_callbacker-win32-${version}.exe
-cp config-release.toml target/package/win32/config.toml
+#mkdir -p target/package/win32/
+#cp target/i686-pc-windows-msvc/release/falcon_bms_callbacker.exe target/package/win32/falcon_bms_callbacker-win32-${version}.exe
+#cp config-release.toml target/package/win32/config.toml
 
-pushd target/package/win32/
-gzip -9 falcon_bms_callbacker-win32-${version}.zip *
-popd
+#pushd target/package/win32/
+#gzip -9 falcon_bms_callbacker-win32-${version}.zip *
+#popd
 
 
 echo "Done."
